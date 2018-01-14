03f521862146868a009199a7554820a84cd84453
Charlie Gerard <charlie.a.gerard@gmail.com>
2018-01-15 08:12:26 +1100

    code sample working with microphone

--- speech-led/server.js
+++ speech-led/server.js
@@ -1,73 +1,3 @@
-// // Imports the Google Cloud client library
-// const speech = require('@google-cloud/speech');
-// const fs = require('fs');
-
-// // Your Google Cloud Platform project ID
-// const projectId = 'LED-speech-test';
-
-// // Creates a client
-// const client = new speech.SpeechClient({
-//   projectId: projectId,
-// });
-
-// // The name of the audio file to transcribe
-// const fileName = './resources/audio.raw';
-
-// // Reads a local audio file and converts it to base64
-// const file = fs.readFileSync(fileName);
-// const audioBytes = file.toString('base64');
-
-// // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-// const audio = {
-//   content: audioBytes,
-// };
-// const config = {
-//   encoding: 'LINEAR16',
-//   sampleRateHertz: 16000,
-//   languageCode: 'en-US',
-// };
-// const request = {
-//   audio: audio,
-//   config: config,
-// };
-
-// // Detects speech in the audio file
-// client
-//   .recognize(request)
-//   .then(data => {
-//     const response = data[0];
-//     const transcription = response.results
-//       .map(result => result.alternatives[0].transcript)
-//       .join('\n');
-//     console.log(`Transcription: ${transcription}`);
-//   })
-//   .catch(err => {
-//     console.error('ERROR:', err);
-//   });
-
-// // Imports the Google Cloud client library.
-// const Storage = require('@google-cloud/storage');
-
-// // Instantiates a client. If you don't specify credentials when constructing
-// // the client, the client library will look for credentials in the
-// // environment.
-// const storage = Storage();
-
-// // Makes an authenticated API request.
-// storage
-//   .getBuckets()
-//   .then((results) => {
-//     const buckets = results[0];
-
-//     console.log('Buckets:');
-//     buckets.forEach((bucket) => {
-//       console.log(bucket.name);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error('ERROR:', err);
-//   });
-
 const record = require('node-record-lpcm16');
 
 // Imports the Google Cloud client library
@@ -79,9 +9,9 @@ const client = new speech.SpeechClient();
 /**
  * TODO(developer): Uncomment the following lines before running the sample.
  */
-const encoding = 'Encoding of the audio file, e.g. LINEAR16';
+const encoding = 'LINEAR16';
 const sampleRateHertz = 16000;
-const languageCode = 'BCP-47 language code, e.g. en-US';
+const languageCode = 'en-US';
 
 const request = {
   config: {

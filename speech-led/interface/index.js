ce747b41f184df90744ac46e100cb0f4b60ffa61
Charlie Gerard <charlie.a.gerard@gmail.com>
2018-02-17 19:00:59 +1100

    start implementing functionality to display letters

--- speech-led/interface/index.js
+++ speech-led/interface/index.js
@@ -1,5 +1,5 @@
 const numLeds = 47; // Real length of strip is 64 but for prototype, only 47 fit on screen.
-const numStrips = 3;
+const numStrips = 10;
 
 var loopTimeOut;
 
@@ -25,23 +25,6 @@ const lightUpColumns = () => {
   })
 }
 
-// lightUpColumns();
-
-const lightUpSingleColumn = () => {
-  for(var x = 0; x < numStrips; x++){
-    firstLedsIndexes.push(x * numLeds)
-  }
-
-  firstLedsIndexes.map((indexInLeds, indexInArray) => {
-    allLeds[indexInLeds].classList.add('lightPink');
-    if(indexInLeds < numLeds * numStrips){
-      loopLightUpSingleColumn(indexInLeds + 1);
-    } else {
-      clearTimeout(loopTimeOut)
-    }
-  })
-}
-
 function loopLightUpColumns (index) {
   if(index < numLeds * 3){
     setTimeout(function(){
@@ -52,18 +35,60 @@ function loopLightUpColumns (index) {
   }
 }
 
-lightUpSingleColumn()
+// lightUpColumns();
 
-function loopLightUpSingleColumn(indexes){
+const lightUpSingleColumn = () => {
+  for(var x = 0; x < numStrips; x++){
+    firstLedsIndexes.push(x * numLeds)
+  }
 
-    loopTimeOut = setTimeout(function(){
-      if(allLeds[indexes-1].classList.contains('lightPink')){
-        console.log('boom')
-        allLeds[indexes-1].classList.remove('lightPink')
-      }
-      allLeds[indexes].classList.add('lightPink');
-      indexes++;
-      loopLightUpSingleColumn(indexes);
-    },50)
+  loopLightUpSingleColumn(firstLedsIndexes);
 }
 
+// lightUpSingleColumn()
+
+let rowCompleted = false;
+
+function loopLightUpSingleColumn(indexes){
+  var newIndexes = [];
+  loopTimeOut = setTimeout(function(){
+    if(!rowCompleted){
+      indexes.map(index => {
+          if(index - 1 >= 0 && !newIndexes.includes(index) && allLeds[index - 1].classList.contains('lightPink')){
+            allLeds[index-1].classList.remove('lightPink');
+          }
+          allLeds[index].classList.add('lightPink');
+          index++;
+          newIndexes.push(index);
+          if(newIndexes.includes(numLeds)){
+            rowCompleted = true;
+            turnOffAllLeds(newIndexes, loopTimeOut);
+          }
+      })
+      loopLightUpSingleColumn(newIndexes);
+    }
+  },300)
+}
+
+function turnOffAllLeds(indexes, timeOutFunctionName){
+  indexes.forEach(index => {
+    if(allLeds[index-1] && allLeds[index-1].classList.contains('lightPink')){
+      allLeds[index-1].classList.remove('lightPink');
+    }
+  })
+  clearTimeout(timeOutFunctionName);
+}
+
+var letterAAray = [2,48,50,94,98,141,145,188,192,235,236,237,238,239,282,286,329,333,376,380,423,427];
+var letterBArray = [0,1,2,47,50,94,97,141,144,188,189,190,235,236,237,282,285,329,332,376,379,423,424,425];
+
+const letters = {
+  'a': letterAAray,
+  'b': letterBArray
+}
+
+function showLetter(letterArray){
+  loopLightUpSingleColumn(letterArray)
+}
+
+showLetter(letterAAray);
